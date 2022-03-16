e9963333edba144ac148a2b0829836010a07fce8
carpaiz <paiz2790@gmail.com>
2022-03-15 21:10:11 -0600

    matriz

--- array/1_array.cpp
+++ array/1_array.cpp
@@ -31,7 +31,14 @@ main(){
 	for(int i=0;i<7;i++){
 		cout<<semana[i]<<endl;
 	}
-	
+	cout<<"--- Bidimensionales---"<<endl;
+	int matriz[2][3]={{10,20,30},{40,50,60}};
+	cout<<"0,0 :"<<matriz[0][0]<<endl;
+	cout<<"0,1 :"<<matriz[0][1]<<endl;
+	cout<<"0,2 :"<<matriz[0][2]<<endl;
+	cout<<"1,0 :"<<matriz[1][0]<<endl;
+	cout<<"1,1 :"<<matriz[1][1]<<endl;
+	cout<<"1,2 :"<<matriz[1][2]<<endl;
 	
 	system("pause");
 }
